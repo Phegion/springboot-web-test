c832d52550e43be85056a3dda913d8b8ebcafac6
Phegion <marcoantoniocastilholeite@gmail.com>
2020-02-15 08:35:15 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/phegion/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.phegion.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.phegion.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>  {
+	
+}
